e9c0421d263f2d08020572d1d0fa60174a0e08f1
Junaid Ahmed <me@junaid.guru>
2023-09-23 22:55:18 +0100

    refactor: fixed database name in cloud and base64 encode password so it can be passed as secret

--- webapp/chatapp/inc/config.php
+++ webapp/chatapp/inc/config.php
@@ -4,5 +4,5 @@ session_start();
 
 define('DB_HOST', getenv('DB_HOST'));
 define('DB_USER', getenv('DB_USER'));
-define('DB_PASS', getenv('DB_PASS'));
+define('DB_PASS', base64_decode(getenv('DB_PASS')));
 define('DB_NAME', getenv('DB_NAME'));
